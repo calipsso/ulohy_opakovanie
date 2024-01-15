37f8194087728fa4488595517630430c38375201
calipsso <kamil.vizy@gmail.com>
2024-01-15 16:23:29 +0100

    conditions

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-x = int(input())
-if x > 0:
-    print(x)
-else:
-    print(-x)
+class Vehicle:
+    def __init__(self, model, year, confort):
+        self.model = model
+        self.year = year
+        self.confort = confort
+
+    def car(self):
+        print(f"Model: {self.model}\nRok vyroby: {self.year}\nVybava: {self.confort}")
+
+auto = Vehicle("Civic", 1985, "elegance")
+
+auto.car()
+
